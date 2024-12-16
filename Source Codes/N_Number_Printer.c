95a2038138a2089e1f7f431c8ef69412622a9a33
muhammad-7-10 <almostakim1@outlook.com>
2024-12-16 20:57:04 +0600

    Added a Source Code

---
+++ Source Codes/N_Number_Printer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World!\nGimme a number to print upto\n");
+    int n;
+    scanf("%d", &n);
+
+    // With do-while loop
+    printf("Using Do-While loop\n");
+    int temp = n; // Create a copy of n for the do-while loop
+    do
+    {
+        temp--;
+        printf("%d\n", temp);
+    } while (temp > 0);
+
+    // For loop
+    printf("Using For loop\n");
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ Source Codes/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+g Do-While loop\n");
+do
+{
+   n--;
+   printf("%d\n",n);
+} while (n>0);
